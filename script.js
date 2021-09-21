a1c7ee974eae06f0853532f9b50328bdd500ae69
Tim Coggins <timcoggins@gmail.com>
2021-09-21 21:30:11 +0200

    First working version
    Fixed problem with the old code, added edge cases checking with alert message, cleaned everything up

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-// Generates a whole integer between limits
+/** 
+   * Removes all the child nodes from a node
+   * 
+   * @param {Element} parent is the parent...
+   * 
+   * **/
+
+ function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+};
+
+
+
+/** 
+ * Returns a random whole integer between the min and max value
+ * 
+ * @param {int} min is the minimum value
+ * @param {int} max is the maximum value
+ * 
+ * **/
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -6,151 +28,210 @@ function getRandomInt(min, max) {
 };
 
 
-// Calculate how many groups based on the number of students and how many people per group
-function byPeople(peoplePerGroup) {
 
-  // List of students
-  const students = [
-    "Tim",
-    "Pierre",
-    "Elli",
-    "Jana",
-    "Josh",
-    "Francis",
-    "Liana",
-    "Gaunthier",
-    "Elisa"
-  ];
+/** 
+* generateByGroups() sorts people into a set number of groups 
+* 
+* @param {string} people is an array of names (strings) to be assigned
+* @param {string} numberOfGroups is the number of groups to sort into
+* 
+* **/
 
-  const people = students;
-  const numberOfGroups = Math.ceil(people.length / peoplePerGroup);
-  return generate(people, numberOfGroups, peoplePerGroup);
-};
+function generateByGroups(people, numberOfGroups) {
 
-
-// Calculate how many groups based on the number of students and how many people per group
-function byGroups(numberOfGroups) {
-
-  // List of students
-  const students = [
-    "Tim",
-    "Pierre",
-    "Elli",
-    "Jana",
-    "Josh",
-    "Francis",
-    "Liana",
-    "Gaunthier",
-    "Elisa"
-  ];
-
-  const people = students;
-  console.log(people);
-  const peoplePerGroup = Math.ceil(people.length / numberOfGroups);
-  return generate(people, numberOfGroups, peoplePerGroup);
-};
-
-
-// Group making code
-function generate(people, numberOfGroups, peoplePerGroup) {
-  let result = [];
-  
-
-  // Loop through the number of groups
-  for (let i = 0; i < numberOfGroups; i++) {
-    let group = [];
-
-    // Add each person to the group
-    for (let x = 0; x < peoplePerGroup; x++) {
-
-      // Randomize a person from the remaining list
-      let selected = getRandomInt(0, people.length);
-
-      // Push that person into the new group array, and remove them from people list
-      group.push(people[selected]);
-      people.splice(selected, 1);
-
-      // If there are no people left to assign, stop looping
-      if (people.length === 0) {
-        break;
-      }
-    }
-    result.push(group);
-
+  // Edge cases
+  if(!people.length) {
+      return {msg: "List of people is empty!"}
   }
+
+  if(numberOfGroups <= 0 || numberOfGroups > people.length) {
+      return {msg: "Number of groups must be in range"};
+  }
+  
+  // Setup the groups as empty arrays
+  let result = [];
+  for(i=0; i < numberOfGroups; i++) {
+      result.push([])
+  }
+
+  // While there are still people to allocate
+  while (people.length) {
+
+      // Loop through each group and add once person at a time
+      for(i=0; i < numberOfGroups; i++) {
+
+          // No more people? Get out of here
+          if (!people.length) {
+              break
+          }
+
+          // Select a random index, push it into the group and remove it from the list of people
+          let selected = getRandomInt(0, people.length);
+          result[i].push(people[selected]);
+          people.splice(selected, 1);
+      }
+  }
+
+  // return the array of groups
   return result;
 };
 
 
 
-// Add code for button
+/** 
+* generateByGroupSize() creates as many as it can with a fixed number of users
+* 
+* @param {string} people is an array of names (strings) to be assigned
+* @param {string} sizeOfGroup is the number of people in each group
+* 
+* **/
+
+function generateByGroupSize(people, sizeOfGroup) {
+
+  // Edge cases
+  if(!people.length) {
+      return {msg: "List of people is empty!"}
+  }
+
+  if(sizeOfGroup <= 0 || sizeOfGroup > people.length) {
+      return {msg: "Number of groups must be in range"};
+  }
+  
+  // Setup 
+  let result = [];
+  let count = 0;
+
+  // While there are still people to allocate
+  while (people.length) {
+      
+      // Create a new group array
+      result.push([])
+
+      // Loop through each group and add once person at a time
+      for(i=0; i < sizeOfGroup; i++) {
+
+          // No more people? Get out of here
+          if (!people.length) {
+              break
+          }
+
+          // Select a random index, push it into the group and remove it from the list of people
+          let selected = getRandomInt(0, people.length);
+          result[count].push(people[selected]);
+          people.splice(selected, 1);
+      }
+      count++;
+      
+  }
+
+  // return the array of groups
+  return result;
+};
+
+
+// HTML DOM queries
 const studentList = document.querySelector("ul");
-
-// NUMBER OF GROUPS
-
 const form1 = document.querySelector("#form1");
+const alertMessage = document.querySelector("#alert");
 
-// When the user clicks on the button
+
+
+/** Submit Button code for the first form */
+
 form1.onsubmit = function (event) {
 
-  // prevent default page reloading
+
+  // List of students (Not sure why but it needs to be here!)
+  const students = [
+      "Tim",
+      "Pierre",
+      "Elli",
+      "Jana",
+      "Josh",
+      "Francis",
+      "Liana",
+      "Gaunthier",
+      "Elisa"
+      ];
+
+  // Prevent default page reloading
   event.preventDefault();
 
-  // remove any previous list items
+  // Remove any previous list items on the DOM
   removeAllChildNodes(studentList);
 
-  // How many groups does the user want? call the function
+  // How many groups does the user want? then call the function
   const number = document.querySelector("#number1");
-  const temp = byGroups(number.value);
+  const temp = generateByGroups(students, number.value);
+
+  // If the generate function generated an error, give it to the user, otherwise reset
+  if (temp.msg) {
+      alertMessage.innerText = temp.msg;
+      return;
+  }
+
+  alertMessage.innerText = "";
 
   // Loop through and create an LI tag for each group
   for (i=0; i<temp.length; i++) {
-    let myElement = document.createElement("li");
-
-    // Join the arrays together with a space between and add to list
-    myElement.innerText = temp[i].join(" ");
-    studentList.appendChild(myElement);
+      let myElement = document.createElement("li");
+  
+      // Join the arrays together with a space between and add to list
+      myElement.innerText = temp[i].join(" ");
+      studentList.appendChild(myElement);
   }
 };
 
 
 
+/** Submit Button code for the second form */
 
-// DEVS PER GROUP
-
-const form2 = document.querySelector("#form2");
-
-// When the user clicks on the button
 form2.onsubmit = function (event) {
 
-  // prevent default page reloading
+
+  // List of students (Not sure why but it needs to be here!)
+  const students = [
+      "Tim",
+      "Pierre",
+      "Elli",
+      "Jana",
+      "Josh",
+      "Francis",
+      "Liana",
+      "Gaunthier",
+      "Elisa"
+      ];
+
+  // Prevent default page reloading
   event.preventDefault();
 
-  // remove any previous list items
+  // Remove any previous list items on the DOM
   removeAllChildNodes(studentList);
 
-  // How many groups does the user want? call the function
+  // How many groups does the user want? then call the function
   const number = document.querySelector("#number2");
-  const temp = byPeople(number.value);
+  const temp = generateByGroupSize(students, number.value);
+
+  // If the generate function generated an error, give it to the user, otherwise reset
+  if (temp.msg) {
+      alertMessage.innerText = temp.msg;
+      return;
+  }
+
+  alertMessage.innerText = "";
 
   // Loop through and create an LI tag for each group
   for (i=0; i<temp.length; i++) {
-    let myElement = document.createElement("li");
-
-    // Join the arrays together with a space between and add to list
-    myElement.innerText = temp[i].join(" ");
-    studentList.appendChild(myElement);
+      let myElement = document.createElement("li");
+  
+      // Join the arrays together with a space between and add to list
+      myElement.innerText = temp[i].join(" ");
+      studentList.appendChild(myElement);
   }
 };
 
 
 
 
-// Used to remove existing nodes
-function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-  }
-};
 
 
